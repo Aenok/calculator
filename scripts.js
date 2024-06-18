b37502d323a86eb2237c577e52fa4d536a1d95c7
Ryan Stutz <ryan.stutz06@gmail.com>
2024-06-18 12:45:50 -0400

    Started adding button functionality. Ensured that scripts waited for the DOM to be fully loaded by wrapping everything in an eventlistener

--- scripts.js
+++ scripts.js
@@ -1,38 +1,61 @@
-let num1;
-let num2;
-let op;
+document.addEventListener("DOMContentLoaded", function(event){
 
-function add(a,b) 
-{
-    return a + b;
-}
+    let num1;
+    let num2;
+    let op;
 
-function subtract(a,b) 
-{
-    return a - b;
-}
+    let display = document.getElementById("display");
+    display.innerText = "0";
 
-function multiply(a,b) 
-{
-    return a * b;
-}
+    let rows = document.getElementsByClassName("row");
 
-function divide(a,b) 
-{
-    return a / b;
-}
+    for(let i = 0; i < rows.length; i++){
+        
+        // button clicking functionality
+        rows[i].addEventListener("click", (e) => {
+            console.log(e.target.innerText);
+        });
 
-function operate(operand1, operand2, operator) 
-{
-    switch(operator) 
-    {
-        case '+':
-            return add(operand1, operand2);
-        case '-':
-            return subtract(operand1, operand2);
-        case '*':
-            return multiply(operand1, operand2);
-        case '/':
-            return divide(operand1, operand2);
+        rows[i].addEventListener("onmousedown", (e) => {
+            if(e.target.classList.contains("sign1")) {
+                e.target.style["background-color"] = "white";
+            }
+        });
     }
-}
+
+    function add(a,b) 
+    {
+        return a + b;
+    }
+
+    function subtract(a,b) 
+    {
+        return a - b;
+    }
+
+    function multiply(a,b) 
+    {
+        return a * b;
+    }
+
+    function divide(a,b) 
+    {
+        return a / b;
+    }
+
+    function operate(operand1, operand2, operator) 
+    {
+        switch(operator) 
+        {
+            case '+':
+                return add(operand1, operand2);
+            case '-':
+                return subtract(operand1, operand2);
+            case '*':
+                return multiply(operand1, operand2);
+            case '/':
+                return divide(operand1, operand2);
+        }
+    }
+
+});
