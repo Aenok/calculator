d9ef524de0cac997d541fe51296e25415b75c145
Ryan Stutz <ryan.stutz06@gmail.com>
2024-06-18 11:21:57 -0400

    Created the basic look of the calculator to better enable functional implementation

--- scripts.js
+++ scripts.js
@@ -1,15 +1,38 @@
-function add(a,b) {
+let num1;
+let num2;
+let op;
+
+function add(a,b) 
+{
     return a + b;
 }
 
-function subtract(a,b) {
+function subtract(a,b) 
+{
     return a - b;
 }
 
-function multiply(a,b) {
+function multiply(a,b) 
+{
     return a * b;
 }
 
-function divide(a,b) {
+function divide(a,b) 
+{
     return a / b;
+}
+
+function operate(operand1, operand2, operator) 
+{
+    switch(operator) 
+    {
+        case '+':
+            return add(operand1, operand2);
+        case '-':
+            return subtract(operand1, operand2);
+        case '*':
+            return multiply(operand1, operand2);
+        case '/':
+            return divide(operand1, operand2);
+    }
 }
